08e535f72c1928654c228ae8ced6824dcf2798ac
93n105 <ernestkwablah19@gmail.com>
2023-06-17 19:35:29 +0000

    loop with bob

--- Case study/Interface design/mypolygon.py
+++ Case study/Interface design/mypolygon.py
@@ -2,19 +2,12 @@ import turtle
 bob = turtle.Turtle()
 print(bob)
 
-bob.fd(100)
-bob.lt(90)
-
-bob.fd(100)
-bob.lt(90)
-
-bob.fd(100)
-bob.lt(90)
-
-bob.fd(100)
+for i in range(4):
+    bob.fd(100)
+    bob.lt(90)
 
 turtle.mainloop()
 
-
+# simple repition
 for i in range(4):
     print("Hello!")
