264ac9a83897f507162b5cd5524f9c6f990d845a
93n105 <ernestkwablah19@gmail.com>
2023-06-19 09:21:42 +0000

    begin exercises

---
+++ Case study/Interface design/exercises.py
@@ -0,0 +1,6 @@
+# Write a function called square that takes a parameter named t, which is a tutle.
+# It should use the turtle to draw a square.
+# Write a function call that passes bob as an argument to square, and then run the program again.
+
+
+def square(t):
