08df0dda91b0c155fc1092330d72428f543683bb
93n105 <ernestkwablah19@gmail.com>
2023-06-20 06:29:28 +0000

    exercise 3 (polygon)

---
+++ Case study/Interface design/exercise3.py
@@ -0,0 +1,19 @@
+# Make a copy of square and change the name to polygon.
+# Add another paraeter named n and modify the body so it draws ana n-sided regular polygon.
+# Hint: The exterior angles of an n-sided regular polygon are 360/n degrees.
+
+import turtle
+bob = turtle.Turtle()
+print(bob)
+
+
+def polygon(t, length, n):
+    angle = 360 / n
+    for i in range(n):
+        t.fd(length)
+        t.lt(angle)
+
+
+polygon(bob, 50, 6)
+
+turtle.mainloop()
