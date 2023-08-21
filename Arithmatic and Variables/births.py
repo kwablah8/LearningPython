5d38dcafda55bcbefb83962838c6da0a9570f26d
93n105 <ernestkwablah19@gmail.com>
2023-08-21 18:09:25 +0000

    calc value of births per day

--- Arithmatic and Variables/births.py
+++ Arithmatic and Variables/births.py
@@ -11,3 +11,10 @@ secs_per_min = 60
 total_secs = secs_per_min * mins_per_hour * \
     hours_per_day * days_per_year * num_years
 print(total_secs)
+
+
+# Create and set value of births_per_min variable
+births_per_min = 250
+
+# calc the value of births per day
+births_per_day = births_per_min * (mins_per_hour * hours_per_day)
