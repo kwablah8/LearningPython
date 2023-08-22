41779ba8bef5f2071dc81411ea21764633e513e0
93n105 <ernestkwablah19@gmail.com>
2023-08-22 10:35:13 +0000

    calculate based on input values

--- Functions/paycheck2.py
+++ Functions/paycheck2.py
@@ -6,3 +6,8 @@ def get_pay_with_more_inputs(num_hours, hourly_wage, tax_bracket):
     # after-tax pay
     pay_aftertax = pay_pretax * (1 - tax_bracket)
     return pay_aftertax
+
+
+# calcullate based on input values
+pay_fultime = get_pay_with_more_inputs(40, 15, 0.12)
+print(pay_fultime)
