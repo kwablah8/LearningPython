f76d1b51fe5e80d3d09c3f3b8d9a5744a01bcf29
93n105 <ernestkwablah19@gmail.com>
2023-08-23 09:25:08 +0000

    variables for four different options

--- Functions/housePriceOptions.py
+++ Functions/housePriceOptions.py
@@ -7,3 +7,15 @@ def get_expected_cost(beds, baths):
 
 house_price = get_expected_cost(1, 1)
 print(house_price)
+
+
+# Use get_expected_cost to get the cost of four different house options
+option_one = get_expected_cost(2, 3)
+option_two = get_expected_cost(3, 2)
+option_three = get_expected_cost(3, 3)
+option_four = get_expected_cost(3, 4)
+
+print(option_one)
+print(option_two)
+print(option_three)
+print(option_four)
