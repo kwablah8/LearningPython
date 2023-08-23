96bf9bc110dd56123d5b396c254b79a0132dc78c
93n105 <ernestkwablah19@gmail.com>
2023-08-23 09:41:29 +0000

    declare project_cost variable

--- Functions/paintCost.py
+++ Functions/paintCost.py
@@ -7,3 +7,9 @@ def get_cost(sqft_walls, sqft_ceiling, sqft_per_gallon, cost_per_gallon):
     gallons_needed = total_sqft / sqft_per_gallon
     cost = gallons_needed * cost_per_gallon
     return cost
+
+
+# Use get_cost to calculate the cost of painting 432 sqft of walls and 144 sqft of ceiling.
+# Assume 1 gallon of paint covers 400 sqft and costs $15.
+project_cost = get_cost(432, 144, 400, 15)
+print(project_cost)
