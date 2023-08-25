9f610fc80dfe6acdd280cb5804badc311b0252b7
93n105 <ernestkwablah19@gmail.com>
2023-08-25 12:55:13 +0000

    define evaluate_temp_using_else funtion

---
+++ Conditions and Conditional Statements/tempEvaluator2.py
@@ -0,0 +1,10 @@
+# program to evalute temperature and tell if person is normal or feverish
+# Using if...else conditional statements
+
+def evaluate_temp_using_else(temp):
+    if temp > 38:
+        message = "You have a fever!"
+    # set else statement
+    else:
+        message = "Your temperature is normal"
+    return message
