424ea2b927bd7db4654e991780b3e899fab23dc6
93n105 <ernestkwablah19@gmail.com>
2023-08-25 19:50:45 +0000

    Define get_cost function

---
+++ Conditions and Conditional Statements/ringCost.py
@@ -0,0 +1,10 @@
+# Program to determine the cost of engraving both a solid gold and a god lated ring.
+# Base cost of solid gold = $100 with $10 for each engraving character.
+# Base cost of goild plated ring = %50 with $7 for each engraving character.
+
+def get_cost(engraving, solid_gold):
+    if solid_gold == True:
+        cost = 100 + 10 * len(engraving)
+    else:
+        cost = 50 + 7 * len(engraving)
+    return cost
