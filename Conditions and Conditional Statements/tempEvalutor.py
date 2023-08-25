4a2ded3b2b6c53c34ee3845c723bc2ad40ff1d58
93n105 <ernestkwablah19@gmail.com>
2023-08-25 12:07:01 +0000

    create and update conditional statements

---
+++ Conditions and Conditional Statements/tempEvalutor.py
@@ -0,0 +1,7 @@
+# program to evalute temperature and tell if person is normal or feverish
+
+
+def evaluate_temp(temp):
+    # set initial message
+    message = "your temperature is normal"
+    return message
