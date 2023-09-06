72a6b3c64ae9ca7cabb2316db01e0b383a0ede77
93n105 <ernestkwablah19@gmail.com>
2023-09-06 11:38:35 +0000

    add new variable

--- Functions/candySmash.py
+++ Functions/candySmash.py
@@ -5,5 +5,8 @@ def to_smash(total_candies, num_of_friends=3):
     return candysmash
 
 
-candy_smashed = to_smash(300)
+candy_smashed = to_smash(308)
 print(candy_smashed)
+
+new_smashed = to_smash(508, 7)
+print(new_smashed)
