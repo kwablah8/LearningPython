b5513cf36ab00c4e5e844e6b243ceb78150901bf
93n105 <ernestkwablah19@gmail.com>
2023-09-07 13:10:33 +0000

    define function

--- booleans and conditional/even_odd_checker.py
+++ booleans and conditional/even_odd_checker.py
@@ -1,1 +1,11 @@
 # Using boolean and conditionals to check if a number input from a user is even or odd
+
+def is_odd(number):
+    message = "The number you entered is an even number"
+    if (number % 2) == 1:
+        message = "The number you entered is an odd number"
+    return message
+
+
+checker = is_odd(55)
+print(checker)
